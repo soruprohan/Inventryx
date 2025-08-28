e1f6db2cde72e9254b84915f2f8c4afd6358cd53
soruprohan <sorupr5@gmail.com>
2025-08-28 19:07:15 +0600

    Admin panel complete

--- resources/views/admin/body/footer.blade.php
+++ resources/views/admin/body/footer.blade.php
@@ -4,7 +4,7 @@
             <div class="col fs-13 text-muted text-center">
                 &copy; <script>
                     document.write(new Date().getFullYear())
-                </script> - Made with <span class="mdi mdi-heart text-danger"></span> by <a href="#!" class="text-reset fw-semibold">Zoyothemes</a>
+                </script> - Made with <span class="mdi mdi-heart text-danger"></span> by <a href="#!" class="text-reset fw-semibold">sorup</a>
             </div>
         </div>
     </div>
