ce0a5502471a6c6518b823b29e60f9eb14bbf841
soruprohan <sorupr5@gmail.com>
2025-10-18 15:21:37 +0600

    Update footer Design

--- resources/views/admin/body/footer.blade.php
+++ resources/views/admin/body/footer.blade.php
@@ -1,10 +1,10 @@
-<footer class="footer">
+<footer class="footer bg-light border-top">
     <div class="container-fluid">
         <div class="row">
-            <div class="col fs-13 text-muted text-center">
-                &copy; <script>
-                    document.write(new Date().getFullYear())
-                </script> - Made with <span class="mdi mdi-heart text-danger"></span> by <a href="#!" class="text-reset fw-semibold">sorup</a>
+            <div class="col-md-6 text-center text-md-start">
+                <p class="mb-0 small text-muted">
+                    &copy; <script>document.write(new Date().getFullYear())</script> Sorup Rohan. All rights reserved.
+                </p>
             </div>
         </div>
     </div>
